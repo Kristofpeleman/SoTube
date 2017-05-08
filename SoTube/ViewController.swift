a6b27d3f9f742d431247156619586ac4b1739679
fatma <ftmbrkts@gmail.com>
2017-05-08 13:28:12 +0200

    print command

--- SoTube/ViewController.swift
+++ SoTube/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hallo")
+        print("This is Fatma calling!")
     }
 
     override func didReceiveMemoryWarning() {
