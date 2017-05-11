387d78f722fd40937647e11ace1ca4469ffed8d9
Robin <regarobin@hotmail.com>
2017-05-11 15:50:40 +0200

    Added Outlets for “forgot password”; Partialy solved “forgot password”-email

--- SoTube/ForgotPasswordViewController.swift
+++ SoTube/ForgotPasswordViewController.swift
@@ -7,29 +7,79 @@
 //
 
 import UIKit
+import Firebase
 
 class ForgotPasswordViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resetPasswordImageView: UIImageView!
+    
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+    @IBAction func resetPassword(_ sender: UIButton) {
+        if emailAddressTextField.text == "" {
+            let alertController = UIAlertController(title: "No Address", message: "Please enter a valid email-address.", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            sendResetPasswordMail()
+        }
     }
-    */
-
+    
+    
+    
+    func sendResetPasswordMail(){
+        FIRAuth.auth()?.sendPasswordReset(withEmail: emailAddressTextField.text!, completion: { (error) in
+            guard let _ = error else {
+                // Dismiss keyboard
+                self.view.endEditing(true)
+                
+                let alertController = UIAlertController(title: "Error", message: "An error occured, please try again later.", preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            
+            // Dismiss keyboard
+            self.view.endEditing(true)
+            
+            
+            let alertController = UIAlertController(title: "Email sent", message: "An email has been sent to you email-address to reset your password.", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        })
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
