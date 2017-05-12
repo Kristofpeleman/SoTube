71582c56af2fdb323259f04e796561d110e15582
kristof <kpeleman@gmail.com>
2017-05-12 16:04:49 +0200

    viewmodel var songs

--- SoTube/AllSongsViewController.swift
+++ SoTube/AllSongsViewController.swift
@@ -12,11 +12,12 @@ import AVFoundation
 
 class AllSongsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let feedURL = "https://api.spotify.com/v1/tracks/1zHlj4dQ8ZAtrayhuDDmkY?"
-    
+//    let feedURL = "https://api.spotify.com/v1/tracks/1zHlj4dQ8ZAtrayhuDDmkY?"
+    let feedURLs = ViewModel().feeds
     let searchURL = "https://api.spotify.com/v1/search?query=Eminem&type=track&market=BE&offset=0&limit=50"
     
-    var song: Song?
+//    var song: Song?
+    var songs: [Song]?
     var audioPlayer: AVAudioPlayer?
 
     
@@ -25,10 +26,12 @@ class AllSongsViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        setSongFromJSONFeed(json: feedURL)
+//        setSongFromJSONFeed(json: feedURL)
+        setSongsFromJSONFeed(jsonData: feedURLs)
     }
     
     override func viewDidAppear(_ animated: Bool) {
+
         self.tableView.reloadData()
     }
 
@@ -41,13 +44,14 @@ class AllSongsViewController: UIViewController, UITableViewDelegate, UITableView
     // MARK: - TableView Datasource Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return (songs?.count) ?? 4
+    
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SongCell", for: indexPath) as! SongTableViewCell
         
-        if let song = self.song {
+        if let song = self.songs?[indexPath.row] {
         cell.artistNameLabel.text = getStringOfArtists(artists: (song.artistNames))
         cell.songTitleLabel.text = song.songTitle
         cell.costLabel.text = String(describing: song.cost)
@@ -60,7 +64,7 @@ class AllSongsViewController: UIViewController, UITableViewDelegate, UITableView
     //MARK: - TableView Delegate Methods
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        playSound(withURL: URL(string: (self.song?.previewURLAssString)!)!)
+        playSound(withURL: URL(string: (self.songs?[indexPath.row].previewURLAssString)!)!)
     }
     
 
@@ -99,31 +103,63 @@ class AllSongsViewController: UIViewController, UITableViewDelegate, UITableView
         return fullListOfArtists
     }
     
-    func setSongFromJSONFeed(json: String) {
+//    func setSongFromJSONFeed(json: String) {
+//        
+//        let request = URLRequest(url: URL(string: json)!)
+//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+//        let task = session.dataTask(with: request) {
+//            data, response, error in
+//            if let jsonData = data,
+//                let feed = (try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)) as? NSDictionary,
+//                let songName = feed.value(forKeyPath: "name") as? String,
+//                let artists = feed.value(forKeyPath: "artists") as? NSArray,
+//                let spotify_ID = feed.value(forKeyPath: "id") as? String,
+//                let preview_url = feed.value(forKeyPath: "preview_url") as? String
+//            {
+//                var allArtists: [String] = []
+//                for dictionary in artists {
+//                    allArtists.append((dictionary as! NSDictionary).value(forKey: "name") as! String? ?? "NOT FOUND")
+//                }
+//                
+//                self.song = Song(songTitle: songName, artistNames: allArtists, spotify_ID: spotify_ID, previewURLAssString: preview_url)
+//                
+//            }
+//        }
+//        
+//        task.resume()
+//    
+//    }
+    
+    func setSongsFromJSONFeed(jsonData: [String]) {
         
-        let request = URLRequest(url: URL(string: json)!)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
-        let task = session.dataTask(with: request) {
-            data, response, error in
-            if let jsonData = data,
-                let feed = (try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)) as? NSDictionary,
-                let songName = feed.value(forKeyPath: "name") as? String,
-                let artists = feed.value(forKeyPath: "artists") as? NSArray,
-                let spotify_ID = feed.value(forKeyPath: "id") as? String,
-                let preview_url = feed.value(forKeyPath: "preview_url") as? String
-            {
-                var allArtists: [String] = []
-                for dictionary in artists {
-                    allArtists.append((dictionary as! NSDictionary).value(forKey: "name") as! String? ?? "NOT FOUND")
+        for json in jsonData {
+            
+            let request = URLRequest(url: URL(string: json)!)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+            let task = session.dataTask(with: request) {
+                data, response, error in
+                if let jsonData = data,
+                    let feed = (try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)) as? NSDictionary,
+                    let songName = feed.value(forKeyPath: "name") as? String,
+                    let artists = feed.value(forKeyPath: "artists") as? NSArray,
+                    let spotify_ID = feed.value(forKeyPath: "id") as? String,
+                    let preview_url = feed.value(forKeyPath: "preview_url") as? String
+                {
+                    var allArtists: [String] = []
+                    for dictionary in artists {
+                        allArtists.append((dictionary as! NSDictionary).value(forKey: "name") as! String? ?? "NOT FOUND")
+                    }
+                    
+                    if let _ = self.songs {
+                    self.songs?.append(Song(songTitle: songName, artistNames: allArtists, spotify_ID: spotify_ID, previewURLAssString: preview_url))
+                    } else {self.songs = [Song(songTitle: songName, artistNames: allArtists, spotify_ID: spotify_ID, previewURLAssString: preview_url)] }
+                    
                 }
-                
-                self.song = Song(songTitle: songName, artistNames: allArtists, spotify_ID: spotify_ID, previewURLAssString: preview_url)
-                
             }
+            
+            task.resume()
         }
         
-        task.resume()
-    
     }
 
 }
