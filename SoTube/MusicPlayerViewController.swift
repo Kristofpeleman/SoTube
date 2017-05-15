9ef5c90b79ea0411cc396dc6684d49677cf7eb83
Robin <regarobin@hotmail.com>
2017-05-15 11:12:10 +0200

    Working progressBar; Working play/pause button; NavigationBarTitel now shows songTitel

--- SoTube/MusicPlayerViewController.swift
+++ SoTube/MusicPlayerViewController.swift
@@ -14,6 +14,7 @@ class MusicPlayerViewController: UIViewController {
     
     var currentSong: Song?
     var audioPlayer: AVAudioPlayer?
+    var displayLink: CADisplayLink?
 
     @IBOutlet weak var navigationSongTitel: UINavigationItem!
     @IBOutlet weak var albumImageView: UIImageView!
@@ -29,6 +30,9 @@ class MusicPlayerViewController: UIViewController {
         super.viewDidLoad()
                 // Do any additional setup after loading the view.
         navigationSongTitel.title = currentSong?.songTitle
+        
+        currentTimeLabel.text = "00:00"
+        endTimeLabel.text = "00:00"
         
     }
 
@@ -46,29 +50,57 @@ class MusicPlayerViewController: UIViewController {
     
     
     @IBAction func playSong(_ sender: UIButton) {
+        
+        
         if audioPlayer != nil {
+            
             if (audioPlayer?.isPlaying)! {
                 audioPlayer?.pause()
                 playOrPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+                
+                displayLink?.invalidate()
             }
             else {
                 audioPlayer?.play()
                 playOrPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+                displayLink = CADisplayLink(target: self, selector: (#selector(MusicPlayerViewController.updateSliderProgress)))
+                displayLink?.add(to: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
             }
         }
         else {
             playSound(withURL: URL(string: (currentSong?.previewURLAssString)!)!)
             playOrPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            displayLink = CADisplayLink(target: self, selector: (#selector(MusicPlayerViewController.updateSliderProgress)))
+            displayLink?.add(to: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
+            
+            if Int((audioPlayer?.duration)!) < 10 {
+                endTimeLabel.text = "\(String(Int((audioPlayer?.duration)!/100))):0\(String(Int((audioPlayer?.duration)!)))"
+            }
+            else {
+                endTimeLabel.text = "\(String(Int((audioPlayer?.duration)!/100))):\(String(Int((audioPlayer?.duration)!)))"
+            }
         }
     }
     
-    
+    func updateSliderProgress(){
+        if audioPlayer != nil {
+            if Int((audioPlayer?.currentTime)!) < 10 {
+                currentTimeLabel.text = "00:0\(String(Int((audioPlayer?.currentTime)!)))"
+            }
+            else {
+                currentTimeLabel.text = "\(String(Int((audioPlayer?.currentTime)!/100))):\(String(Int((audioPlayer?.currentTime)!)))"
+            }
+            let progress = (audioPlayer?.currentTime)! / (audioPlayer?.duration)!
+            songProgressView.setProgress(Float(progress), animated: false)
+        }
+    }
     
     @IBAction func nextSong(_ sender: UIButton) {
     }
     
     
     @IBAction func back(_ sender: UIBarButtonItem) {
+        audioPlayer = nil
         dismiss(animated: true, completion: nil)
     }
     
