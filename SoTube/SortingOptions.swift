7eba60e25e0aaf77353d8ba26087ab5361f39e48
Robin <regarobin@hotmail.com>
2017-05-17 10:33:43 +0200

    Added documentation to SortingOptions

--- SoTube/SortingOptions.swift
+++ SoTube/SortingOptions.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
+// Dragged an Object into storyBoard that uses this .swift file; Also has to follow UIPickerViewDataSource-protocol
 class SortingOptions: NSObject, UIPickerViewDataSource {
     
+    // The values inside our pickerView (text per row)
     let values = ["None", "Artist (A-Z)", "Artist (Z-A)", "Song Title (A-Z)", "Song Title (Z-A)"]
     
+    // Amount of scrollers in pickerView (0 = nothing)
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
+    // Amount of rows inside the pickerView's component
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return values.count
     }
