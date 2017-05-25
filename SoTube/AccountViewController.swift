f61fbeac931d5ad862508099e195fab99b08f0a2
Kristof Peleman <kpeleman@gmail.com>
2017-05-26 01:25:30 +0200

    Login/ logout implemented for AccountVC

--- SoTube/AccountViewController.swift
+++ SoTube/AccountViewController.swift
@@ -14,28 +14,92 @@ class AccountViewController: TopMediaViewController, LoginViewControllerDelegate
     // MARK: - Global variables and constants
     
     var shared = Shared.current
-
+    
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var loginButton: UIBarButtonItem!
+    
+    
+    // MARK: - UIViewController functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        print(FIRAuth.auth()?.currentUser ?? "NO FIRUser")
+        print(FIRAuth.auth()?.currentUser?.displayName ?? "NO FIRUser displayName")
+        
+        if let _ = shared.user {
+            loginButton.title = "Log out"
+            
+            print(self.shared.user?.fireBaseID ?? "NO FIREBASE ID")
+            print(self.shared.user?.userName ?? "NO USERNAME")
+            print(self.shared.user?.emailAddress ?? "NO EMAIL")
+            print(self.shared.user?.points ?? "NO POINTS")
+            
+        } else {
+            loginButton.title = "Log in"
+        }
+        
+    }
+    
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "loginSegue" {
+            if let destinationVC = segue.destination as? LogInViewController {
+                destinationVC.delegate = self
+            }
+        }
+        
     }
-    */
+    
+    // An override function when performing a segue
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        // Check the value of "identifier"
+        switch identifier {
+        // If the value is "loginSegue"
+        case "loginSegue":
+            // If "logInButton"'s title is "Log out"
+            if loginButton.title == "Log out" {
+                
+                // Get the user that is logged in his/her online-status from FireBase
+                let currentOnlineUserReference = FIRDatabase.database().reference(withPath: "online users/\(self.shared.user!.fireBaseID)")
+                // Remove the value of the online-status (make it go offline)
+                currentOnlineUserReference.removeValue()
+                
+                // Since the value isn't in FireBase anymore, we must delete it localy
+                self.shared.user = nil
+
+                // Change "logInButton"'s title to "Log in"
+                loginButton.title = "Log in"
+                
+                // Leave the function with the return and don't perform the segue
+                return false
+            }
+                // If "logInButton"'s title isn't "Log out"
+            else {
+                // Perform the segue
+                return true
+            }
+        // If the identifier's value isn't any of the above: perform Segue
+        default: return true
+        }
+    }
+ 
     
     
     // MARK: - LoginViewControllerDelegate methods
